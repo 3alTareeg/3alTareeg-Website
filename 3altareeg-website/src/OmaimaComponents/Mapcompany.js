8b6ac824ca6a9dce81d057306d63e1959be98e17
omaima <omemah.sh@gmail.com>
2019-09-10 18:03:41 +0300

    ix conflict 1

--- 3altareeg-website/src/OmaimaComponents/Mapcompany.js
+++ 3altareeg-website/src/OmaimaComponents/Mapcompany.js
@@ -20,6 +20,7 @@ export default class Mapcompany extends Component {
 
   
     updateCompany=(request,id)=>{
+      //today
       if(request===false)
           request=true
       console.log(request)
